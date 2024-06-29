50dfdaa600d5f9978086745dbbce5fb4ae86a885
@hejj <hejj@tsintergy.com>
2024-06-29 13:43:25 +0800

    chore: prettier

--- word/tools-check.js
+++ word/tools-check.js
@@ -1,17 +1,21 @@
-const { execSync } = require('child_process');
+const { execSync } = require('child_process')
 
 try {
-  // 检查pnpm是否安装
-  const pnpmVersion = execSync('pnpm --version').toString().trim();
-  console.log(`pnpm is installed. Version: ${pnpmVersion}`);
+	// 检查pnpm是否安装
+	// const pnpmVersion = execSync('pnpm --version').toString().trim()
+	// console.log(`pnpm is installed. Version: ${pnpmVersion}`)
 
-  // todo
-  // const pnpmVersion = execSync('pnpm --version').toString().trim();
-  // console.log(`pnpm is installed. Version: ${pnpmVersion}`);
+	const rimrafExists = execSync('pnpm ', {
+		encoding: 'utf8'
+	}).includes('rimraf')
+					console.log(`rimraf] exists: ${rimrafExists}`);
 
-  // 检查rimraf是否存在于node_modules/.bin中
-  // const rimrafExists = execSync('ls node_modules/.bin | grep rimraf', { encoding: 'utf8' }).includes('rimraf');
-  // console.log(`rimraf exists: ${rimrafExists}`);
+	// const pnpmVersion = execSync('pnpm --version').toString().trim();
+	// console.log(`pnpm is installed. Version: ${pnpmVersion}`);
+
+	// 检查rimraf是否存在于node_modules/.bin中
+	// const rimrafExists = execSync('ls node_modules/.bin | grep rimraf', { encoding: 'utf8' }).includes('rimraf');
+	// console.log(`rimraf exists: ${rimrafExists}`);
 } catch (error) {
-  console.error(`${error.message} One or both tools may not be installed.`);
+	console.error(`${error.message} One or both tools may not be installed.`)
 }
